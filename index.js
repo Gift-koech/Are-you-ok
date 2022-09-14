d71c106653f898e22f2d36fc82e97561c64fcc27
Gift-koech <koechgift93@gmail.com>
2022-09-14 10:33:16 +0300

    modified  the style.css file

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ search = document.querySelector("#searchbar")
            <div class = "user flex-child">
 
          <p>${article.source}</p>
-          <p>${article.title}</p><
+          <p>${article.title}</p>
            </div>
             `;
         })
@@ -50,6 +50,24 @@ post.addEventListener("click", function(){
     document.getElementById("unordered").appendChild(li);
  
 });
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
     
     
